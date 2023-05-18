d83f0146cae33f40e2a9a5f96e2af2be74133ea2
fubi927 <119533087+fubi927@users.noreply.github.com>
2023-05-18 22:57:07 +0200

    ForwardSpeed added
    fwdSpeed über "transform.up" statt über velocity. Vllt muss man den max-speed noch über velocity eingrenzen.

--- GravityForce/Assets/PlayerMovement.cs
+++ GravityForce/Assets/PlayerMovement.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D ship;
+
+    private Vector2 fwdSpeed = new Vector2(0, 8);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ship = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            forward();
+        }
 
+    }
 
+    private void forward()
+    {
+        Vector2 fwd = transform.up;
+        ship.AddForce(fwd);
     }
 }
