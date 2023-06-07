e20f08ab03d5ca42ac76ffe38d7b730204412c8d
fubi927 <119533087+fubi927@users.noreply.github.com>
2023-06-07 22:36:16 +0200

    TileMap Test + Scene Lvl01

--- GravityForce/Assets/PlayerMovement.cs
+++ GravityForce/Assets/PlayerMovement.cs
@@ -42,11 +42,11 @@ public class PlayerMovement : MonoBehaviour
             audioThrust.volume = 0;
         }
         
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && (ship.velocity != Vector2.zero))
         {
             angle += Time.deltaTime * rotationSpeed;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && (ship.velocity != Vector2.zero))
         {
             angle += -Time.deltaTime * rotationSpeed;
         }
